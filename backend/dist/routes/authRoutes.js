7ec8507294321f9a02573b8438b0e3e2dc9a46ab
AvadhutK01 <kelaskaravadhut052@gmail.com>
2026-01-21 23:10:15 +0530

    refac: removed dist folder

--- backend/dist/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authController_1 = require("../controller/authController");
-const protect_1 = require("../utils/protect");
-const router = (0, express_1.Router)();
-router.post('/google', authController_1.googleAuth);
-router.get('/profile', protect_1.protect, authController_1.getProfile);
-exports.default = router;
--- backend/dist/routes/courseRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const courseController_1 = require("../controller/courseController");
-const authMiddleware_1 = require("../middleware/authMiddleware");
-const router = (0, express_1.Router)();
-router.post('/input', authMiddleware_1.authMiddleware, courseController_1.recieveInput);
-exports.default = router;
